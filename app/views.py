805202cd9c9f233c46dade6281d6f3f2fe16e1c3
Tasheka Martin <tasheka_m@yahoo.com>
2018-02-13 21:35:23 +0000

    Profile

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -19,10 +19,24 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/about/')
+@app.route('/about')
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+    
+@app.route('/profile/')
+def profile():
+    """Render the website's about page."""
+    return render_template('about.html', name="Mary Jane")
+    
+def format_date_joined():
+    now = datetime.datetime.now() # today's date
+    date_joined = datetime.date(2018, 2, 7) # a specific date
+    ## date and time representation
+    print "Current date & time " + now.strftime("%c")
+    
+    ## Only month and year date representation
+    print "Joined " + date_joined.strftime("%B, %Y") 
 
 
 ###
